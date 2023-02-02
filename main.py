c1d7cbb54969ac4d6a78c8d96960e00eecc10472
rojcandraz <andraz.rojc@caretronic.com>
2023-02-02 15:12:49 +0100

    Writing a working script

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import os
 
 def write_the_files() :
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-    print(desktop)
+    path = desktop + "\omicron_script.bat"
+    print(path)
+    f = open(path, "a")
+    f.write("ping 192.168.2.1")
+    f.close();
+
 
 
 # Press the green button in the gutter to run the script.
