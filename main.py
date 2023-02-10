35cf435525f8fe545505e7fc8b4932cd8497ae76
rojcandraz <andraz.rojc@caretronic.com>
2023-02-10 11:01:22 +0100

    Pisanje skript

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
+import subprocess
 
 def write_the_files() :
-    desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-    path = desktop + "\omicron_script.bat"
-    print(path)
-    f = open(path, "a")
-    f.write("ping 192.168.2.1")
-    f.close();
+    desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop\pings')
+    paths = []
+    for i in range(1000) :
+        paths.append(desktop + "\omicron_script" + str(i) + ".bat")
+        print(paths[i])
+        f = open(paths[i], "w")
+        command = "ping 192.168.4.90 -n 100 -l " + str(60000+i)
+        f.write(command)
+        f.close();
 
+    for i in range(1000) :
+        print(str(i))
+        subprocess.run([paths[i]])
 
 
 # Press the green button in the gutter to run the script.
