b4b3d084620b1e1d19f0d89ac21b690a9948ebd6
rojcandraz <andraz.rojc@caretronic.com>
2023-02-10 13:02:02 +0100

    Ustvaril sem razred pingonizer

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import os
 import subprocess
+import threading
+import time
 
-def write_the_files() :
+
+def write_the_files(N) :
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop\pings')
     paths = []
-    for i in range(1000) :
+    for i in range(N) :
         paths.append(desktop + "\omicron_script" + str(i) + ".bat")
         print(paths[i])
         f = open(paths[i], "w")
-        command = "ping 192.168.4.90 -n 100 -l " + str(60000+i)
+        command = "ping 192.168.4.90 -w 3000 -n 4 -l " + str(60000+i)
         f.write(command)
         f.close();
 
-    for i in range(1000) :
-        print(str(i))
-        subprocess.run([paths[i]])
+class Pingonizer :
+
+    def run_the_thread (self, n) :
+        desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop\pings')
+        path = desktop + "\omicron_script" + str(n) + ".bat"
+        t = threading.Thread(target=self.run_the_process, args=(path,))
+        t.start()
+
+    def run_the_process(self, path) :
+        subprocess.run([path])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    write_the_files()
+
+
+
+write_the_files(1000)
+
+p = Pingonizer()
+p.run_the_thread(1)
+p.run_the_thread(2)
+p.run_the_thread(3)
+p.run_the_thread(4)
+p.run_the_thread(5)
